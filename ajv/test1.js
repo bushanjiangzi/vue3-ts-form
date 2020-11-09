3192a391c3cb6e518e402563b0ca5e430a42cedb
bushanjiangzi <grjiangzi@163.com>
2020-11-10 00:21:55 +0800

    新增笔记

--- ajv/test1.js
+++ ajv/test1.js
@@ -2,11 +2,49 @@
 const Ajv = require('ajv')
 
 const schema = {
-  type: 'string',
-  minLength: 10
+  type: 'object',
+  properties: {
+    name: {
+      type: 'string',
+      maxLength: 10
+    },
+    age: {
+      type: 'number'
+    },
+    email: {
+      type: 'string',
+      format: 'email'
+    },
+    test: {
+      type: 'string',
+      format: 'test'
+    },
+    love: {
+      type: 'array',
+      items: {
+        type: 'string'
+      }
+    },
+    isWorker: {
+      type: 'boolean'
+    }
+  },
+  required: ['name', 'age'] // 必填项
 }
 
-const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
+const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+// 自定义format
+ajv.addFormat('test', (data) => {
+  console.log(data, '>>>---<<<')
+  return data === 'yes'
+})
 const validate = ajv.compile(schema)
-const valid = validate('data')
+const valid = validate({
+  name: 'jiang',
+  age: 18,
+  email: 'grjiangzi@163.com',
+  test: 'yes',
+  love: ['eat', 'read'],
+  isWorker: true
+})
 if (!valid) console.log(validate.errors)
